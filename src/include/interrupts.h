58900e024a3462358649749141aa8fbb696c818d
colget <colget@hotmail.com>
2025-10-17 12:56:21 +0100

    Fixed scancode_to_ascii static declaration conflict - Amoeba 0.2.1

--- src/include/interrupts.h
+++ src/include/interrupts.h
@@ -1,40 +1,77 @@
 #ifndef INTERRUPTS_H
 #define INTERRUPTS_H
 
-// Interrupt Descriptor Table structure
+// IDT structure
 struct idt_entry {
-    unsigned short base_lo;    // Low 16 bits of handler address
-    unsigned short sel;        // Kernel segment selector (0x08)
-    unsigned char always0;     // Always 0
-    unsigned char flags;       // Flags (described below)
-    unsigned short base_hi;    // High 16 bits of handler address
+    unsigned short base_lo;
+    unsigned short sel;
+    unsigned char always0;
+    unsigned char flags;
+    unsigned short base_hi;
 } __attribute__((packed));
 
-// IDT pointer structure
+// IDT pointer
 struct idt_ptr {
-    unsigned short limit;      // Size of IDT - 1
-    unsigned int   base;       // Address of IDT
+    unsigned short limit;
+    unsigned int base;
 } __attribute__((packed));
 
-// Flags for IDT entries
-#define IDT_FLAG_PRESENT     0x80    // Present in memory
-#define IDT_FLAG_RING0       0x00    // Kernel mode
-#define IDT_FLAG_INTGATE     0x06    // Interrupt gate
+// Flags
+#define IDT_FLAG_PRESENT     0x80
+#define IDT_FLAG_RING0       0x00
+#define IDT_FLAG_INTGATE     0x0E
 #define IDT_FLAG_FLAGS       (IDT_FLAG_PRESENT | IDT_FLAG_RING0 | IDT_FLAG_INTGATE)
 
-// Function declarations
-void init_idt(void);
-void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
-// PIC I/O ports
+// PIC ports
 #define PIC1_COMMAND  0x20
 #define PIC1_DATA     0x21
 #define PIC2_COMMAND  0xA0
 #define PIC2_DATA     0xA1
 #define PIC_EOI       0x20
 
+// Function declarations
+void init_idt(void);
+void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
 void pic_init(void);
 void pic_ack(unsigned int irq);
 void outb(unsigned short port, unsigned char val);
 unsigned char inb(unsigned short port);
+void interrupt_handler_33(void);
+void exception_handler(unsigned int interrupt_no, unsigned int error_code);
+
+// ISRs
+extern void isr0(void);
+extern void isr1(void);
+extern void isr2(void);
+extern void isr3(void);
+extern void isr4(void);
+extern void isr5(void);
+extern void isr6(void);
+extern void isr7(void);
+extern void isr8(void);
+extern void isr9(void);
+extern void isr10(void);
+extern void isr11(void);
+extern void isr12(void);
+extern void isr13(void);
+extern void isr14(void);
+extern void isr15(void);
+extern void isr16(void);
+extern void isr17(void);
+extern void isr18(void);
+extern void isr19(void);
+extern void isr20(void);
+extern void isr21(void);
+extern void isr22(void);
+extern void isr23(void);
+extern void isr24(void);
+extern void isr25(void);
+extern void isr26(void);
+extern void isr27(void);
+extern void isr28(void);
+extern void isr29(void);
+extern void isr30(void);
+extern void isr31(void);
+extern void isr33(void);
 
 #endif
